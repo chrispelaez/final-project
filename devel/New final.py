dcce38384774055fff7cbe79d6f3f0b0a4bf6687
chrispelaez <cpelaez5@mail.valenciacollege.edu>
2016-04-27 21:02:33 -0400

    Final project
    I don’t know what happened but the only file that worked was the design
    doc. I don’t know what went wrong but the other files would 404 when
    ever I would try to open them. hopefully this helps out

---
+++ devel/New final.py
@@ -0,0 +1,84 @@
+#This is the final version of the game
+#updated rock paper scissors game now has 12 options enter options using letters as your input.
+#all avalible options
+#r=rock
+#p=paper
+#s=scissors
+#l=lightsaber
+#b=blaster
+#i=ion grenade
+#w=wolf
+#h=human
+#g=goblin
+#a=air
+#f=fire
+#e=earth
+#Hidden in the code there is a way to get 1 free point.....you may need it but its up to you to figure it out.
+
+#Instructions for Running my Project
+#step 1 open a new file on python
+#step 2 select run module from the options menu
+#step 3 enjoy!
+
+import random
+choicelist = ["Rock", "Paper", "Scissors","lightsaber","blaster","ion grenade","wolf","human","goblin","air","fire","earth"]
+wins = 0
+losses = 0
+run = True
+while run == True:
+    user_input = input(str("r for rock, p for paper, and s for scissors.\n"))
+    if user_input == "r":
+        user_input = 0
+    elif user_input == "p":
+        user_input = 1
+    elif user_input == "s":
+        user_input = 2
+    elif user_input == "l":
+        user_input = 3
+    elif user_input == "b":
+        user_input = 4
+    elif user_input == "i":
+        user_input = 5
+    elif user_input == "w":
+        user_input = 6
+    elif user_input == "h":
+        user_input = 7
+    elif user_input == "g":
+        user_input = 8
+    elif user_input == "a":
+        user_input = 9
+    elif user_input == "f":
+        user_input = 10
+    elif user_input == "e":
+        user_input = 11
+    else:
+        print("Please enter a valid choice.")
+        if user_input == "joust":
+            wins = wins + 1
+            continue
+           
+    comp_input = random.randint(0, 11)
+    print(choicelist[user_input], "vs.", choicelist[comp_input])
+    if user_input == comp_input:
+        print("Joust.")
+        losses += -1
+    elif ((user_input - comp_input + 3) % 3 == 1):
+        print("Win.")
+        wins = wins + 1
+    else:
+        print("Lose.")
+        losses += 1
+    print("Player: ", wins, "\nComputer: ", losses)
+    again = True
+    while again == True:
+        playagain = input(str("Play again (y or n)? "))
+        if playagain == "n":
+            print("Goodbye!")
+            run = False
+            again = False
+        elif playagain == "y":
+            print("Let's play again.")
+            again = False
+        else:
+            print("Please enter a valid choice.")
+            continue
--- devel/design doc.py
+++ devel/design doc.py
@@ -1,12 +1,12 @@
 #I wanted to make a rock paper scissors game that ran on the concept of RPS 15. 
 #Rps 15 is a rock paper scissors game with 15 options for players to use this game uses 12.
 #Growing up rock paper scissors was the game that decided almost any problems that would arise between friends.
-#I choose to make a list for all avalible options that both the player and computer will use. 
+#I choose to make a list for all available options that both the player and computer will use. 
 #I had a version prior to this one where if i gave it options in terms of a function the game would crash. 
 #I wanted to create a game that kept score, and had a while loop to keep the game from ending as well as had hidden options for the players to use.
 #One of the easter eggs for the game was if the player typed in "joust" the player would gain 1 point.
-#If the computer and player had a draw the word joust would apear and 1 point would be subtracted from the computer.
-#growing up when ever my friends and I would play Rock, Paper, Sissors whenever there was a draw the first one to shout out the word "joust" would win the bout.
+#If the computer and player had a draw the word joust would appear and 1 point would be subtracted from the computer.
+#growing up when ever my friends and I would play Rock, Paper, Scissors whenever there was a draw the first one to shout out the word "joust" would win the bout.
 #In order to bring that concept into the game whenever there is a draw the computer losses one point.
 #the rules of the game are simple: 
 #Both players start out with 0 points.
@@ -16,7 +16,7 @@
 #If the computers choice is greater than the users choice then the computer wins.
 #After each bout the player is given the choice to continue by pressing "y" or pressing "n" to quit.
 #If the player presses "n" then the game ends.
-#The items are listed in the rockpaper scissors format.
+#The items are listed in the rock paper scissors format.
 #For example Fire beats air but earth beats fire air beats earth.
 #With all options in play the game would look like This
 #Fire beats air but human beats fire while goblin beats human.
